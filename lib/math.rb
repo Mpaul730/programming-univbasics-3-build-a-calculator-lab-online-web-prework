90318324bce3b66587ae874cbf0620157960f235
Mark Paul <mpaul730@gmail.com>
2019-09-26 21:18:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,21 @@
 def addition(num1, num2)
-
+sum = 5 + 4
 end
 
 def subtraction(num1, num2)
-
+difference = 10 - 5
 end
 
 def division(num1, num2)
-
+quotient = 50 / 2 
 end
 
 def multiplication(num1, num2)
-
+product = 4 * 30
 end
 
 def modulo(num1, num2)
-
+remainder 34 / 5
 end
 
 def square_root(num)
